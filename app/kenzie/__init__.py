5049219f7daf7485163b59ed8b13740867be5df5
Laudemir <nlaudemir@gmail.com>
2022-01-06 15:29:29 -0300

    correçoes

--- app/kenzie/__init__.py
+++ app/kenzie/__init__.py
@@ -1,5 +1,22 @@
+import os
+from dotenv import load_dotenv
+
 from .files import allowed_file
 from .files import save_file
 from .files import dowload_file
 from .files import zip_file
 from .files import get_files
+
+load_dotenv()
+FILES_DIRECTORY = os.getenv('FILES_DIRECTORY')
+if not os.path.exists(FILES_DIRECTORY):
+    os.mkdir(FILES_DIRECTORY)
+
+if not os.path.exists(f'{FILES_DIRECTORY}/gif'):
+    os.mkdir(f'{FILES_DIRECTORY}/gif')
+
+if not os.path.exists(f'{FILES_DIRECTORY}/png'):
+    os.mkdir(f'{FILES_DIRECTORY}/png')
+
+if not os.path.exists(f'{FILES_DIRECTORY}/jpg'):
+    os.mkdir(f'{FILES_DIRECTORY}/jpg')
